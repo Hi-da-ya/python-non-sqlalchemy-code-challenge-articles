32687248a094996d5cf3bcc9c4e500f694196465
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-06-02 22:47:02 +0300

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,4 +1,5 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
@@ -7,14 +8,36 @@ class Article:
         if not (5 <= len(title) <=50):
             raise ValueError("Title must be between 5 and 50 characters")
         self.__title = title
-    #setting author name property
+        Article.all.append(self)
+    #setting article title property
     @property
     def title(self):
         return self.__title 
 
     @title.setter
     def title(self, value):
-        raise AttributeError("Name cannot be modified after instantiation")
+        raise AttributeError("Title cannot be modified after instantiation")
+    
+    #setting author property
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if not(isinstance(author, Author)):
+            raise ValueError("Object must be an instance of Author class")
+        self._author = author
+
+    #setting magazine property
+    @property
+    def magazine(self):
+        return self._magazine
+    @magazine.setter
+    def magazine(self, magazine):
+        if not isinstance(magazine, Magazine):
+            raise Exception("Object must be an instance of Magazine class")
+        self._magazine = magazine    
            
         
 class Author:
@@ -22,6 +45,7 @@ class Author:
           if not isinstance(name, str) or len(name) == 0:
             raise ValueError("Name must be a non-empty string")
           self.__name = name
+          
     #setting author name property
     @property
     def name(self):
@@ -32,16 +56,24 @@ class Author:
         raise AttributeError("Name cannot be modified after instantiation")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        magazine = set()
+        #iterate through author's articles and add them to the magazines he has contributed to
+        for article in self.articles():
+            magazine.add(article.magazine)
+        return list(magazine)    
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self,magazine, title)
+        return new_article
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        else:
+            return list({article.magazine.category for article in self.articles()})
 
 class Magazine:
     def __init__(self, name, category):
@@ -55,6 +87,7 @@ class Magazine:
     def name(self, value):
         if isinstance(value, str) and 2<= len(value) <= 16:
             self._name = value
+
     #setting category property
     @property
     def category(self):
@@ -65,16 +98,23 @@ class Magazine:
             self._category = category        
 
             
-
-
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        authors = set()
+        for article in self.articles():
+            authors.add(article.author)
+        return list(authors)    
 
     def article_titles(self):
-        pass
+        if not self.articles:
+            return None
+        return list(article.title for article in self.articles())
 
     def contributing_authors(self):
-        pass
+        contribution = {}
+        for article in self.articles:
+            if contribution.get(article.author, 0) > 2:
+                contribution[article.author] += 1
+        return [author for author, count in contribution.items() if count >= 2]
