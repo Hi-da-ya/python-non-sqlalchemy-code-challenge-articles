f2e22e106ed25912ab926537bc9d5486864f88d6
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-06-02 09:04:25 +0300

    initialization done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,11 +2,34 @@ class Article:
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string")
+        if not (5 <= len(title) <=50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        self.__title = title
+    #setting author name property
+    @property
+    def title(self):
+        return self.__title 
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Name cannot be modified after instantiation")
+           
         
 class Author:
     def __init__(self, name):
-        self.name = name
+          if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+          self.__name = name
+    #setting author name property
+    @property
+    def name(self):
+        return self.__name 
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name cannot be modified after instantiation")
 
     def articles(self):
         pass
@@ -24,6 +47,25 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    #setting name property
+    @property
+    def name(self):
+        return self._name 
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2<= len(value) <= 16:
+            self._name = value
+    #setting category property
+    @property
+    def category(self):
+        return self._category
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category        
+
+            
+
 
     def articles(self):
         pass
